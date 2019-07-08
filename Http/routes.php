301b405dc3dcca6b627972fb4311a69e7560d02a
zeloras <zeloras@gmail.com>
2019-07-08 09:17:06 +0300

    permission to static

--- Http/routes.php
+++ Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['web', 'permission:admin_access'], 'prefix' => getAdminPrefix('content')], function () {
+Route::group(['middleware' => ['web', 'permission:' . \Gcms::MAIN_ADMIN_PERMISSION], 'prefix' => getAdminPrefix('content')], function () {
     Route::group(['middleware' => ['permission:modules_content_admin_list']], function () {
         Route::get('{type}', 'GeekCms\Content\Http\Controllers\AdminController@index')
             ->name('content')
